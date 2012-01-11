2f8edd2b9d757476d9eb3fbb2bbb93452cc61bf3
Prof. MAAD <himself@prof-maad.org>
2012-01-11 18:05:02 +0100

    argh

---
+++ src/libmaelcum.h
@@ -0,0 +1,24 @@
+/* libmaelcum, library to generate signed urls for Amazon Cloudfront
+ * 
+ * Copyright (C) 2012, Max Wolter aka Prof. MAAD
+ *
+ * libmaelcum is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+	
+ * libmaelcum is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+	
+ * You should have received a copy of the GNU General Public License
+ * along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+# ifndef LIBMAELCUM_H
+# define LIBMAELCUM_H
+
+struct maelcum_ctx;
+
+# endif /*LIBMAELCUM_H*/
